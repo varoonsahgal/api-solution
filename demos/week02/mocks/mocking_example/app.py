f1386a03812768764aa669d5ae0130b9bc621bef
Allen Sanders <asanders@gamuttechnologysvcs.com>
2022-10-26 08:38:50 -0400

    update mocks demo

--- demos/week02/mocks/mocking_example/app.py
+++ demos/week02/mocks/mocking_example/app.py
@@ -1,7 +1,15 @@
 from services.service import Service
 from controllers.controller import Controller
 
-quantity = int(input('Enter quantity: '))
-unit_cost = float(input('Enter unit cost: '))
-controller = Controller(Service())
-print("${:,.2f}".format(controller.get_discounted_price(quantity, unit_cost)))
+def main():
+    '''
+    Launch point for application.
+    '''
+
+    quantity = int(input('Enter quantity: '))
+    unit_cost = float(input('Enter unit cost: '))
+    controller = Controller(Service())
+    print("${:,.2f}".format(controller.get_discounted_price(quantity, unit_cost)))
+
+if __name__ == "__main__":
+    main()
